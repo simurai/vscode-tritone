d79363e2fa1a4431f134fd0fff08110ba1854af3
simurai <simulus@gmail.com>
2021-01-01 14:53:46 +0900

    Add more themes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
 const fs = require("fs").promises;
 const buildTheme = require("./theme");
 
-const darkTheme = buildTheme({
-  theme: "dark",
-  name: "TriTone Dark",
+// Example
+// const darkTheme = buildTheme({
+//   name         : 'TriTone Dark',
+//   accent1Color : 'hsl(190, 88%, 70%)',
+//   accent2Color : 'hsl(159, 99%, 68%)',
+//   bgColor      : 'hsl(249, 13%, 19%)',
+//   fgSaturate   : 0.5,
+//   fgContrast   : 0.5,
+//   bgContrast   : 0.5,
+// });
+
+const deepsea = buildTheme({
+  name         : 'TriTone DeepSea',
+  accent1Color : 'hsl(190, 88%, 70%)',
+  accent2Color : 'hsl(159, 99%, 68%)',
+  bgColor      : 'hsl(210, 24%, 15%)',
+  fgSaturate   : 0.5,
+  fgContrast   : 0.5,
+  bgContrast   : 0.5,
 });
 
+const dark = buildTheme({
+  name         : 'TriTone Dark',
+  accent1Color : 'hsl(50, 92%, 66%)',
+  accent2Color : 'hsl(29, 87%, 63%)',
+  bgColor      : 'hsl(249, 13%, 19%)',
+  fgSaturate   : 0.5,
+  fgContrast   : 0.5,
+  bgContrast   : 0.5,
+});
+
+const light = buildTheme({
+  name         : 'TriTone Light',
+  accent1Color : 'hsl(150, 99%, 30%)',
+  accent2Color : 'hsl(240, 99%, 25%)',
+  bgColor      : 'hsl(200, 100%, 99%)',
+  fgSaturate   : 0.5,
+  fgContrast   : 0.5,
+  bgContrast   : 0.5,
+});
 
 fs.mkdir("./themes", { recursive: true })
   .then(() => Promise.all([
-    fs.writeFile("./themes/dark.json", JSON.stringify(darkTheme, null, 2)),
+    fs.writeFile("./themes/deepsea.json", JSON.stringify(deepsea, null, 2)),
+    fs.writeFile("./themes/dark.json", JSON.stringify(dark, null, 2)),
+    fs.writeFile("./themes/light.json", JSON.stringify(light, null, 2)),
   ]))
   .catch(() => process.exit(1))
--- src/theme.js
+++ src/theme.js
@@ -1,88 +1,72 @@
 const chroma = require("chroma-js");
 
-// Theme ----------------------------------------------------------
-
-// Dark mode
-const bgColor      = 'hsl(249, 13%, 19%)'; // Background
-// const bgColor      = 'hsl(210, 24%, 15%)'; // Background
-// const bgColor      = 'hsl(240, 12%, 16%)'; // Background
-// const bgColor      = 'hsl(27, 10%, 18%)'; // Background
-// const bgColor      = 'hsl(120, 3%, 17%)'; // Background
-
-const accent1Color = 'hsl(190, 88%, 70%)'; // Accent 1
-const accent2Color = 'hsl(159, 99%, 68%)'; // Accent 2
-
-// Light mode
-// const bgColor      = 'hsl(200, 100%, 99%)'; // Background white
-// const accent1Color = 'hsl(150, 99%, 30%)'; // Accent 1
-// const accent2Color = 'hsl(240, 99%, 25%)'; // Accent 2
-
-const fgSaturate = 0.5;
-const fgContrast = 0.5;
-const bgContrast = 0.5;
-
-// Config ----------------------------------------------------------
-
 const colorMode = 'lab';
-const mode = chroma(bgColor).luminance() > 0.5 ? 'light' : 'dark';
 
-// Color modes ----------------------------------------------------------
+function buildTheme({ ...args }) {
 
-if (mode == 'light') {
-  var modeColor = 'black';
-  var bdScale = 6;
-  var fgContrastPadding = 0.5 - fgContrast; // Adjust range of the scale
-  var fgColor = chroma.mix(bgColor, modeColor, 0.25 + fgContrast).saturate(fgSaturate); // Based on background
-  var bgUColor = chroma(bgColor).darken(bgContrast); // darken both directions
-  var bgDColor = chroma(bgColor).darken(bgContrast);
+  // Config
+  const accent1Color = args.accent1Color || 'hsl(320, 88%, 80%)'; // Accent 1
+  const accent2Color = args.accent2Color || 'hsl(120, 88%, 84%)'; // Accent 2
+  const bgColor      = args.bgColor      || 'hsl(200, 3%, 16%)'; // Background
+  const fgSaturate   = args.fgSaturate   || 0.5;
+  const fgContrast   = args.fgContrast   || 0.5;
+  const bgContrast   = args.bgContrast   || 0.5;
 
-} else if (mode == 'dark') {
-  var modeColor = 'white';
-  var bdScale = bgContrast < 0.4 ? 0 : 6;
-  var fgContrastPadding = 0.5 - fgContrast; // Adjust range of the scale
-  var fgColor = chroma.mix(bgColor, modeColor, fgContrast).saturate(fgSaturate); // Based on background
-  var bgUColor = chroma(bgColor).brighten(bgContrast);
-  var bgDColor = chroma(bgColor).darken(bgContrast);
-}
+  // Theme type
+  const mode = chroma(bgColor).luminance() > 0.5 ? 'light' : 'dark';
 
-// UI
+  if (mode == 'light') {
+    var modeColor = 'black';
+    var bdScale = 6;
+    var fgContrastPadding = 0.5 - fgContrast; // Adjust range of the scale
+    var fgColor = chroma.mix(bgColor, modeColor, 0.25 + fgContrast).saturate(fgSaturate); // Based on background
+    var bgUColor = chroma(bgColor).darken(bgContrast); // darken both directions
+    var bgDColor = chroma(bgColor).darken(bgContrast);
 
-const fg = chroma.scale([ modeColor, fgColor, bgColor ]).padding([0.02 + fgContrastPadding, 0.4]).mode(colorMode).colors(4); // padding cuts off the edges
-const bg = chroma.scale([ bgUColor, bgColor, bgDColor ]).mode(colorMode).colors(7);
+  } else if (mode == 'dark') {
+    var modeColor = 'white';
+    var bdScale = bgContrast < 0.4 ? 0 : 6;
+    var fgContrastPadding = 0.5 - fgContrast; // Adjust range of the scale
+    var fgColor = chroma.mix(bgColor, modeColor, fgContrast).saturate(fgSaturate); // Based on background
+    var bgUColor = chroma(bgColor).brighten(bgContrast);
+    var bgDColor = chroma(bgColor).darken(bgContrast);
+  }
 
-// Syntax
+  // UI
+  const fg = chroma.scale([ modeColor, fgColor, bgColor ]).padding([0.02 + fgContrastPadding, 0.4]).mode(colorMode).colors(4); // padding cuts off the edges
+  const bg = chroma.scale([ bgUColor, bgColor, bgDColor ]).mode(colorMode).colors(7);
 
-const unoColor  = fgColor.saturate(fgSaturate * 2); // increased saturation
-const duoColor  = accent1Color; // Accent 1
-const triColor  = accent2Color; // Accent 2
+  // Syntax
+  const unoColor  = fgColor.saturate(fgSaturate * 2); // increased saturation
+  const duoColor  = accent1Color; // Accent 1
+  const triColor  = accent2Color; // Accent 2
 
-const uno = chroma.scale([ fg[0], unoColor, bgColor ]).correctLightness().padding([fgContrastPadding, 0.25]).mode(colorMode).colors(5);
-const duo = chroma.scale([ duoColor, bgColor ]).padding([fgContrastPadding, 0.4]).mode(colorMode).colors(3);
-const tri = chroma.scale([ triColor, bgColor ]).padding([fgContrastPadding, 0.4]).mode(colorMode).colors(3);
+  const uno = chroma.scale([ fg[0], unoColor, bgColor ]).correctLightness().padding([fgContrastPadding, 0.25]).mode(colorMode).colors(5);
+  const duo = chroma.scale([ duoColor, bgColor ]).padding([fgContrastPadding, 0.4]).mode(colorMode).colors(3);
+  const tri = chroma.scale([ triColor, bgColor ]).padding([fgContrastPadding, 0.4]).mode(colorMode).colors(3);
 
-// UI Scale -------------------------------------------------------------
+  // UI Scale
+  const color = {
+    'fg1u' : fg[0],
+    'fg'   : fg[1],
+    'fg1d' : fg[2],
+    'fg2d' : fg[3],
 
-const color = {
-  'fg1u' : fg[0],
-  'fg'   : fg[1],
-  'fg1d' : fg[2],
-  'fg2d' : fg[3],
+    'bg3u' : bg[0],
+    'bg2u' : bg[1],
+    'bg1u' : bg[2],
+    'bg'   : bg[3],
+    'bg1d' : bg[4],
+    'bg2d' : bg[5],
 
-  'bg3u' : bg[0],
-  'bg2u' : bg[1],
-  'bg1u' : bg[2],
-  'bg'   : bg[3],
-  'bg1d' : bg[4],
-  'bg2d' : bg[5],
+    'bd'   : bg[bdScale], // 6 or 0
+  }
 
-  'bd'   : bg[bdScale], // 6 or 0
-}
+  // Output -------------------------------------------------------------
 
-// Theme -------------------------------------------------------------
-
-function buildTheme({ name }) {
   return {
-    name: name,
+    name: args.name,
+    type: mode,
     colors: {
       foreground           : color.fg,
       descriptionForeground: color.fg1d,
