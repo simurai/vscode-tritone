e101bd14b52a81ffc680bc3369fccdbf4b2d9e2d
simurai <simulus@gmail.com>
2021-01-02 17:44:57 +0900

    Add GitHub themes

--- src/index.js
+++ src/index.js
@@ -1,64 +1,40 @@
 const fs = require("fs").promises;
 const buildTheme = require("./theme");
 
-const DarkForest = buildTheme({
-  name         : 'TriTone DarkForest',
-  accent1Color : 'hsl(70, 88%, 77%)',
-  accent2Color : 'hsl(178, 99%, 60%)',
-  bgColor      : 'hsl(120, 3%, 17%)',
-  fgSaturate   : 0.5,
-  fgContrast   : 0.4,
-  bgContrast   : 0.2,
-});
-
-const DarkEarth = buildTheme({
-  name         : 'TriTone DarkEarth',
-  accent1Color : 'hsl(41, 98%, 68%)',
-  accent2Color : 'hsl(70, 88%, 70%)',
-  bgColor      : 'hsl(27, 10%, 18%)',
-});
-
-const DarkSpace = buildTheme({
-  name         : 'TriTone DarkSpace',
-  accent1Color : 'hsl(19, 96%, 67%)',
-  accent2Color : 'hsl(330, 88%, 70%)',
-  bgColor      : 'hsl(240, 12%, 16%)',
-  fgSaturate   : 0.8,
+const DarkDimmed = buildTheme({
+  name         : 'TriTone DarkDimmed',
+  accent1Color : 'hsl(5, 100%, 80%)',
+  accent2Color : 'hsl(208, 100%, 74%)',
+  bgColor      : 'hsl(215, 21%, 11%)',
+  fgSaturate   : 0.4,
   fgContrast   : 0.5,
-  bgContrast   : 0.6,
+  bgContrast   : 0.5,
 });
 
-const DarkSea = buildTheme({
-  name         : 'TriTone DarkSea',
-  accent1Color : 'hsl(160, 97%, 71%)',
-  accent2Color : 'hsl(190, 88%, 70%)',
-  bgColor      : 'hsl(210, 24%, 15%)',
-});
-
-const DarkSky = buildTheme({
-  name         : 'TriTone DarkSky',
-  accent1Color : 'hsl(30, 98%, 84%)',
-  accent2Color : 'hsl(60, 92%, 66%)',
-  bgColor      : 'hsl(249, 13%, 19%)',
-  fgSaturate   : 0.7,
-  fgContrast   : 0.6,
-  bgContrast   : 0.2,
+const Dark = buildTheme({
+  name         : 'TriTone Dark',
+  accent1Color : '#FFA198',
+  accent2Color : '#79C0FF',
+  bgColor      : '#0D1117',
+  fgSaturate   : 0.6,
+  fgContrast   : 0.5,
+  bgContrast   : 0.39,
 });
 
 const Light = buildTheme({
   name         : 'TriTone Light',
-  accent1Color : 'hsl(240, 99%, 25%)',
-  accent2Color : 'hsl(150, 99%, 30%)',
-  bgColor      : 'hsl(200, 100%, 99%)',
+  accent1Color : 'hsl(212, 94%, 20%)',
+  accent2Color : 'hsl(354, 66%, 54%)',
+  bgColor      : 'hsl(210, 100%, 99.99%)',
+  fgSaturate   : 0.3,
+  fgContrast   : 0.6,
+  bgContrast   : 0.35,
 });
 
 fs.mkdir("./themes", { recursive: true })
   .then(() => Promise.all([
-    fs.writeFile("./themes/dark-forest.json", JSON.stringify(DarkForest, null, 2)),
-    fs.writeFile("./themes/dark-earth.json", JSON.stringify(DarkEarth, null, 2)),
-    fs.writeFile("./themes/dark-space.json", JSON.stringify(DarkSpace, null, 2)),
-    fs.writeFile("./themes/dark-sea.json", JSON.stringify(DarkSea, null, 2)),
-    fs.writeFile("./themes/dark-sky.json", JSON.stringify(DarkSky, null, 2)),
+    fs.writeFile("./themes/dark-dimmed.json", JSON.stringify(DarkDimmed, null, 2)),
+    fs.writeFile("./themes/dark.json", JSON.stringify(Dark, null, 2)),
     fs.writeFile("./themes/light.json", JSON.stringify(Light, null, 2)),
   ]))
   .catch(() => process.exit(1))
